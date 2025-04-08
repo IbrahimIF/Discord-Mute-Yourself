26e062d8cf0ed96edf490a49c0def7ea869844eb
Ibrahim Farrah <ibrahimfarrah30@gmail.com>
2025-04-08 14:52:49 +0100

    install and testing of pyaudio
    Changes:
    - Installed pyaudio
    Used for recording my voice and creating a .wav file.
    
    - Swapped handsome.wav audio for the recorded wave file.

--- main.py
+++ main.py
@@ -1,9 +1,37 @@
 import os
 import speech_recognition as sr
+import pyaudio
+
+import wave
+import sys
+
+
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1 if sys.platform == 'darwin' else 2
+RATE = 24100
+RECORD_SECONDS = 5
+
+with wave.open('output.wav', 'wb') as wf:
+    p = pyaudio.PyAudio()
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)
+
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+
+    print('Recording...')
+    for _ in range(0, RATE // CHUNK * RECORD_SECONDS):
+        wf.writeframes(stream.read(CHUNK))
+    print('Done')
+
+    stream.close()
+    p.terminate()
+
 
 def takeCommand():
     print("Begin")
-    burger = sr.AudioFile('handsome.wav')
+    burger = sr.AudioFile('output.wav')
     r = sr.Recognizer()
     with burger as source:
         print("Listening....")
